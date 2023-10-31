bb029d8a5ac1b91fb8de5d6067058b71c4042e3c
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-31 15:40:47 +0100

    add v-show for off-canva

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@ const { createApp } = Vue
     createApp({
         data(){
             return{
-                
+                showCanvaB: false,
             }
         },
         methods: {
-            
+            showCanva(){
+                this.showCanvaB = true 
+            },
+            closeCanva(){
+                this.showCanvaB = false 
+            },
         },
         mounted(){
-             
+            
         }
     }).mount('#app')
