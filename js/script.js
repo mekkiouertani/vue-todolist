d12e738a2aeb22719808b3e5d545bbe848e37734
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-31 15:53:52 +0100

    add v-for

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ const { createApp } = Vue
         data(){
             return{
                 showCanvaB: false,
+                people: [
+                    { 
+                        id: 1,
+                        name: "Light Yagami",
+                        done: true
+                    },
+                    { 
+                        id: 2,
+                        name: "Eren Jaeger",
+                        done: false
+                    },
+                    { 
+                        id: 3,
+                        name: "Kaido",
+                        done: true
+                    },
+                ]
             }
         },
         methods: {
